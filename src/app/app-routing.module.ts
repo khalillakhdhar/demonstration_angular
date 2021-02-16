ad6b22a635e113eedf9be5ebf0115414c03fbe39
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-02-16 19:18:39 +0100

    sync

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProduitComponent } from './produit/produit.component';
 
 const routes: Routes = [
-  
+  {path:"", component:ProduitComponent},
 ];
 
 @NgModule({
