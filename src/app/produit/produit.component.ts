6131ee23a537a330b8c625d609a246f5c9c31c04
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-02-16 19:56:00 +0100

    project

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produit.component.css']
 })
 export class ProduitComponent implements OnInit {
-
+  prix: number;
+  quantite: number;
+  titre: string;
+  taxes=0;
   constructor() { }
 
   ngOnInit(): void {
   }
+taxe()
+{
+  this.taxes=(this.prix*this.quantite)*0.19;
 
+
+}
 }
